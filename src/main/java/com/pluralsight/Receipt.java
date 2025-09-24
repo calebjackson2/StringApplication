e8cfd7c77ae069db6a317a7f0d45973382e0b2b3
caleb jackson <jacksoncaleb740@gmail.com>
2025-09-24 14:34:15 -0400

    VariebleApp and Receipt Project

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+        String itemName = "IBUYPOWER ";
+        double itemPrice = 849.90;
+        int quanity = 9;
+        System.out.println(itemName + itemPrice + quanity);
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/pluralsight/VariebleApp.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class VariebleApp {
+    public static void main(String[] args) {
+        String favoritecolor = "Blue";
+        System.out.println(favoritecolor);
+         int firstnumber = 2025;
+        System.out.println(firstnumber);
+        char middleInitial = 'G';
+        System.out.println(middleInitial);
+        Boolean hasPets = false;
+        System.out.println(hasPets);
+        String nicemessage = "You guys are the best";
+        System.out.println(nicemessage);
+        int daysoftheweek = 7;
+        System.out.println(daysoftheweek);
+        double coffeePrice = 3.50;
+        System.out.println(coffeePrice);
+        char favoriteLetter = 'c';
+        System.out.println("My favorite letter is " + favoriteLetter);
+        boolean isRaining = true;
+        System.out.println("Is it raining? " +isRaining);
+    }
+}
